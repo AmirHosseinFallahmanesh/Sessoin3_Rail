cc105870e7e0265668c6752959f8d0ffb4c36e4b
AmirHosseinFallahmanesh <54832303+AmirHosseinFallahmanesh@users.noreply.github.com>
2021-12-25 10:54:45 +0330

    e

---
+++ Demo11_Random/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Demo11_Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random randomMachine = new Random();
+            int a=randomMachine.Next(1, 100);
+            Console.WriteLine(a);
+                
+                
+        }
+    }
+}
